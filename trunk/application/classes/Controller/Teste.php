48b6df86e9ef3f62c030484a6691e5f58cf6a21e
rubs33 <rubs33@07cd6032-15af-d9ca-68c8-0c1344c3da38>
2014-06-03 23:18:21 +0000

    Padronizacao do codigo segudo as convencoes do Kohana.

--- trunk/application/classes/Controller/Teste.php
+++ trunk/application/classes/Controller/Teste.php
@@ -1,11 +1,23 @@
 <?php
+/**
+ * Controller de teste
+ * @author Rubens Takiguti Ribeiro <rubs33@gmail.com>
+ */
 class Controller_Teste extends Controller_Geral {
 
-    public function action_testar() {
-        $this->definir_title('Teste Inicial');
-        $this->adicionar_meta(array('name' => 'robots', 'content' => 'index,follow'));
+	/**
+	 * Teste
+	 * @return void
+	 */
+	public function action_testar()
+	{
+		$this->definir_title('Teste Inicial');
+		$this->adicionar_meta(array(
+			'name'    => 'robots',
+			'content' => 'index,follow'
+		));
 
-        $view = View::Factory('teste/testar');
-        $this->template->content = $view;
-    }
+		$view = View::Factory('teste/testar');
+		$this->template->content = $view;
+	}
 }
