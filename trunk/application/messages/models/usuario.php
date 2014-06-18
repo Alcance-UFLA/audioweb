8d606296793b0b76fd7582aa21821b4d78edbf79
rubs33 <rubs33@07cd6032-15af-d9ca-68c8-0c1344c3da38>
2014-06-18 02:51:10 +0000

    Ajuste de mensagem de erro

--- trunk/application/messages/models/usuario.php
+++ trunk/application/messages/models/usuario.php
@@ -10,6 +10,7 @@ return array(
 		'not_empty' => 'Faltou preencher o campo E-mail',
 		'min_length' => 'O campo E-mail deve ter o tamanho mínimo :param2.',
 		'max_length' => 'O campo E-mail deve ter o tamanho máximo :param2.',
+		'unique' => 'Já existe um usuário com este e-mail.'
 	),
 	'senha' => array(
 		'not_empty' => 'Faltou preencher o campo Senha'
