025086f8c4ea98e68d8eac83bd4f491ab8bfe537
rubs33 <rubs33@07cd6032-15af-d9ca-68c8-0c1344c3da38>
2014-08-31 18:17:01 +0000

    Melhorias no formulario de autenticacao.

--- trunk/application/classes/Controller/Autenticacao/Autenticar.php
+++ trunk/application/classes/Controller/Autenticacao/Autenticar.php
@@ -1,27 +1,36 @@
 <?php
 /**
  * Action para realizar autenticação do usuário
- * @author Gustavo Araújo <kustavo@gmail.com>
+ * @author Rubens Takiguti Ribeiro <rubs33@gmail.com>
  */
 class Controller_Autenticacao_Autenticar extends Controller_Geral {
 
+	/**
+	 * Exibe o formulário de autenticação.
+	 * @return void
+	 */
 	public function action_index()
 	{
 		$this->definir_title('Autenticação do Usuário');
 		$this->definir_description('Página para logar usuários no sistema AudioWeb.');
 		$this->definir_canonical(URL::site('autenticacao/autenticar'));
 
-		$mensagens = Session::instance()->get_once('flash_message', array());
+		$dados = array();
+		$dados['mensagens'] = Session::instance()->get_once('flash_message', array());
+		$dados['form_autenticacao'] = $this->request->query('form_autenticacao');
 
-		$this->template->content = View::Factory('autenticacao/autenticar/index');
-		$this->template->content->set('mensagens', $mensagens);
+		$this->template->content = View::Factory('autenticacao/autenticar/index', $dados);
 	}
 
+	/**
+	 * Recebe os dados de autenticação e redireciona para a página principal.
+	 * @return void
+	 */
 	public function action_login()
 	{
 		if ($this->request->method() != 'POST')
 		{
-			HTTP::redirect('autenticacao/autenticar' . HTTP::query(array()));
+			HTTP::redirect('autenticacao/autenticar' . URL::query(array()));
 		}
 
 		$usuario = Auth::instance()->login(
@@ -32,9 +41,14 @@ class Controller_Autenticacao_Autenticar extends Controller_Geral {
 
 		if ( ! $usuario)
 		{
+			$form_autenticacao = array(
+				'email' => $this->request->post('email'),
+				'lembrar' => $this->request->post('lembrar') ? '1' : NULL
+			);
+
 			$mensagens = array('erro' => 'Usuário ou senha estão incorretos.');
 			Session::instance()->set('flash_message', $mensagens);
-			HTTP::redirect('autenticacao/autenticar');
+			HTTP::redirect('autenticacao/autenticar' . URL::query(array('form_autenticacao' => $form_autenticacao)));
 		}
 
 		HTTP::redirect('principal');
