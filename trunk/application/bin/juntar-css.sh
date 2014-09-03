9c411c02e5b2b3ad8854b05b6044e6857e0e5ad4
rubs33 <rubs33@07cd6032-15af-d9ca-68c8-0c1344c3da38>
2014-09-03 01:51:16 +0000

    Ajuste do composer.json e dos scripts de minificacao de CSS e JS.

--- trunk/application/bin/juntar-css.sh
+++ trunk/application/bin/juntar-css.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
-echo "Compactando arquivos CSS"
-java -jar ./yuicompressor.jar --type css -v ../../htdocs/css/bootstrap.css -o ../../htdocs/css/bootstrap.min.css
-java -jar ./yuicompressor.jar --type css -v ../../htdocs/css/bootstrap-theme.css -o ../../htdocs/css/bootstrap-theme.min.css
-echo "OK"
+CURDIR=$(dirname $(realpath "$0"))
+HTDOCS=$(realpath "${CURDIR}/../../htdocs")
 
 echo "Juntando arquivos CSS"
-cat ../../htdocs/css/bootstrap.min.css ../../htdocs/css/bootstrap-theme.min.css > ../../htdocs/css/tb.css
+cat "${HTDOCS}/css/bootstrap.css" "${HTDOCS}/css/bootstrap-theme.css" "${HTDOCS}/css/audioweb.css"  > "${HTDOCS}/css/tb.css"
+
+echo "Compactando arquivos CSS"
+for ARQ in $(find "$HTDOCS" -name "*.css")
+do
+    ARQMIN="${ARQ%%.*}.min.css"
+    EXTENSAO="${ARQ#*.}"
+    if [ $EXTENSAO = "css" ]
+    then
+        echo "Compactando: $ARQ"
+        java -jar "${CURDIR}/yuicompressor.jar" --type css --charset "UTF-8" "$ARQ" -o "$ARQMIN"
+    fi
+done
+
 echo "OK"
