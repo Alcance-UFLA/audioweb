32162d6328b4c69bddb84ad09d8ccecffe6d71a0
rubs33 <rubs33@07cd6032-15af-d9ca-68c8-0c1344c3da38>
2014-09-09 22:57:20 +0000

    Criacao de metodo para obter restricao da licenca.

--- trunk/application/schema/insert.sql
+++ trunk/application/schema/insert.sql
@@ -7,3 +7,8 @@ INSERT INTO contas (id_conta, id_licenca) VALUES
 
 INSERT INTO usuarios (nome, email, senha, id_conta) VALUES
   ('Administrador', 'admin@audioweb.com.br', '78be0f381152733304d0102195899ea793c2d498f8bf25344b107ee792f3df88', 1);
+
+INSERT INTO restricoes_aplicacao (chave, nome, valor_padrao) VALUES
+  ('CONTA.LIMITE_USUARIOS', 'Limite de usuários na conta', '1'),
+  ('AUDIOIMAGEM.CADASTRAR_IMAGEM', 'Cadastrar/Alterar imagens no AudioImagem', 'true'),
+  ('AUDIOIMAGEM.VISUALIZAR_IMAGEM', 'Visualizar imagem no AudioImagem', 'true');
