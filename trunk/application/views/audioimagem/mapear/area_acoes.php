4c6419c9c9ef10b708c318ce2998d925eef2d98d
rubs33 <rubs33@07cd6032-15af-d9ca-68c8-0c1344c3da38>
2014-10-20 02:12:45 +0000

    Melhorias no layout da ferramenta de mapeamento.

--- trunk/application/views/audioimagem/mapear/area_acoes.php
+++ trunk/application/views/audioimagem/mapear/area_acoes.php
@@ -9,46 +9,36 @@ if (isset($dados_url['host'])) {
 }
 $cookie_cor_selecao['expires'] = 30;
 ?>
-<div class="area-acoes well">
-	<ul class="list-inline">
-		<li class="tipo-regiao">
-			<p>
-				Tipo de Região:
-				&nbsp;
-				<span data-toggle="buttons" class="btn-group">
-					<label class="btn btn-default btn-poly">
-						<input type="radio" value="poly" name="btn_tipo_regiao"> Polígono
-					</label>
-					<label class="btn btn-default btn-rect">
-						<input type="radio" value="rect" name="btn_tipo_regiao"> Retângulo
-					</label>
-					<label class="btn btn-default btn-circle">
-						<input type="radio" value="circle" name="btn_tipo_regiao"> Círculo
-					</label>
-				</span>
-			</p>
-		</li>
-		<li class="cor">
-			<p>
-				Cor de Seleção:
-				&nbsp;
-				<input type="color" id="cor-selecao" name="cor_selecao" value="#FF0000" size="10" maxlength="7" data-cookie-path="<?= HTML::chars($cookie_cor_selecao['path']) ?>" data-cookie-domain="<?= HTML::chars($cookie_cor_selecao['domain']) ?>" data-cookie-expires="<?= HTML::chars($cookie_cor_selecao['expires']) ?>" />
-			</p>
-		</li>
-		<li class="acoes">
-			<p>
-				Ações:
-				&nbsp;
-				<button class="btn btn-warning btn-voltar-ponto" type="button"><i class="glyphicon glyphicon-step-backward"></i> Voltar ponto</button>
-				&nbsp;
-				<button class="btn btn-danger btn-limpar-regiao" type="button"><i class="glyphicon glyphicon-trash"></i> Limpar região</button>
-				&nbsp;
-				<button class="btn btn-success btn-salvar-regiao" type="button" data-toggle="modal" data-target="#modal-form-regiao"><i class="glyphicon glyphicon-ok"></i> Salvar região</button>
+<div class="panel panel-default area-acoes">
+	<div class="panel-heading">
+		<h2 class="panel-title"><i class="glyphicon glyphicon-cog"></i> Opções para mapeamento</h2>
+	</div>
+	<div class="panel-body">
+		<div class="tipo-regiao form-group">
+			<div>Tipo de Região:</div>
+			<div data-toggle="buttons" class="btn-group">
+				<?php foreach ($form_imagem['lista_tipo_regiao'] as $id_tipo_regiao => $nome_tipo_regiao): ?>
+				<label class="btn btn-default btn-<?= $id_tipo_regiao?> <?= $form_imagem['dados']['regiao']['tipo_regiao'] == $id_tipo_regiao ? 'active' : '' ?>">
+					<?= Form::radio('btn_tipo_regiao', $id_tipo_regiao, $form_imagem['dados']['regiao']['tipo_regiao'] == $id_tipo_regiao) ?>
+					<?= HTML::chars($nome_tipo_regiao) ?>
+				</label>
+				<?php endforeach ?>
+			</div>
+		</div>
+		<div class="cor form-group">
+			<div>Cor de Seleção:</div>
+			<div><input type="color" id="cor-selecao" name="cor_selecao" value="#FF0000" size="10" maxlength="7" data-cookie-path="<?= HTML::chars($cookie_cor_selecao['path']) ?>" data-cookie-domain="<?= HTML::chars($cookie_cor_selecao['domain']) ?>" data-cookie-expires="<?= HTML::chars($cookie_cor_selecao['expires']) ?>" /></div>
+		</div>
+		<div class="acoes form-group">
+			<div>Ações:</div>
+			<div>
+				<button class="btn btn-warning btn-block btn-voltar-ponto" type="button"><i class="glyphicon glyphicon-step-backward"></i> Voltar ponto</button>
+				<button class="btn btn-danger btn-block btn-limpar-regiao" type="button"><i class="glyphicon glyphicon-trash"></i> Limpar região</button>
+				<button class="btn btn-success btn-block btn-salvar-regiao" type="button" data-toggle="modal" data-target="#modal-form-regiao"><i class="glyphicon glyphicon-ok"></i> Salvar região</button>
 				<?php if ($form_imagem['dados']['regiao']['id_imagem_regiao']): ?>
-				&nbsp;
-				<a class="btn btn-default btn-cancelar-alteracao" href="<?= Route::url('alterar', array('directory' => 'audioimagem', 'controller' => 'mapear', 'id' => $form_imagem['dados']['imagem']['id_imagem'])) ?>"><i class="glyphicon glyphicon-remove"></i> Cancelar Alteração</a>
+				<a class="btn btn-default btn-block btn-cancelar-alteracao" href="<?= Route::url('alterar', array('directory' => 'audioimagem', 'controller' => 'mapear', 'id' => $form_imagem['dados']['imagem']['id_imagem'])) ?>"><i class="glyphicon glyphicon-remove"></i> Cancelar Alteração</a>
 				<?php endif ?>
-			</p>
-		</li>
-	</ul>
+			</div>
+		</div>
+	</div>
 </div>
