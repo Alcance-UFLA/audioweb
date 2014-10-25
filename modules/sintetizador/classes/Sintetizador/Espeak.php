c9589e7463c61f5d3b04ca5d0777182317ede6c1
Rubens Takiguti Ribeiro <rubs33@gmail.com>
2014-10-25 01:25:58 +0000

    Criacao de modulo sintetizador de fala a partir de texto.

---
+++ modules/sintetizador/classes/Sintetizador/Espeak.php
@@ -0,0 +1,6 @@
+<?php
+/**
+ * Driver text-to-speach que usa o programa espeak + lame.
+ * @author Rubens Takiguti Ribeiro <rubs33@gmail.com>
+ */
+class Sintetizador_Espeak extends Kohana_Sintetizador_Espeak {}
