9e581e7facf717b9b5923f07b0378728800f075d
rubs33 <rubs33@07cd6032-15af-d9ca-68c8-0c1344c3da38>
2014-12-21 18:56:49 +0000

    Ajuste da paginacao para exibir mais links.

--- trunk/application/classes/Helper/Paginacao.php
+++ trunk/application/classes/Helper/Paginacao.php
@@ -15,11 +15,45 @@ class Helper_Paginacao {
 	{
 		$paginacao = self::montar_paginacao($paginacao);
 
+		// Calcular quantidade de botoes na esquerda e direita
+		if (isset($paginacao['quantidade_botoes']) && $paginacao['quantidade_botoes'] > 0)
+		{
+			$quantidade_botoes = min($paginacao['quantidade_botoes'], $paginacao['ultima_pagina']);
+
+			$esquerda = intval($quantidade_botoes / 2);
+			$direita  = $esquerda;
+			if ($quantidade_botoes % 2 == 0)
+			{
+				$direita -= 1;
+			}
+
+			if ($paginacao['pagina'] - $esquerda < 1)
+			{
+				$dif = $esquerda + 1 - $paginacao['pagina'];
+				$esquerda -= $dif;
+				$direita  += $dif;
+			}
+			elseif ($paginacao['pagina'] + $direita >= $paginacao['ultima_pagina'])
+			{
+				$dif = $paginacao['pagina'] + $direita - $paginacao['ultima_pagina'];
+				$direita  -= $dif;
+				$esquerda += $dif;
+			}
+		}
+		else
+		{
+			$quantidade_botoes = 0;
+			$esquerda = 0;
+			$direita = 0;
+		}
+
+		// Preparar estilos
 		$estilos_default = array(
 			'class' => 'pagination'
 		);
 		$estilos = array_merge($estilos_default, $estilos);
 
+		// Exibir paginacao
 		if ($paginacao['ultima_pagina'] <= 1)
 		{
 			return '';
@@ -28,6 +62,8 @@ class Helper_Paginacao {
 		$html = '<div>';
 		$html .= '<span class="sr-only">Paginação:</span>';
 		$html .= sprintf('<div class="%s">', $estilos['class']);
+
+		// Pagina anterior
 		if ($paginacao['pagina'] > 1)
 		{
 			$html .= sprintf(
@@ -40,11 +76,59 @@ class Helper_Paginacao {
 			$html .= '<li class="disabled"><span title="Página Anterior">&laquo;</span></li>';
 		}
 
+		// Primeira pagina
+		if ($paginacao['pagina'] - $esquerda > 1) {
+			$html .= sprintf(
+				'<li><a href="%s" title="Primeira página"><span class="sr-only">Página</span> %s</a></li>',
+				call_user_func($paginacao['callback_link'], $paginacao, 1),
+				number_format(1, 0, ',', '.')
+			);
+			if ($paginacao['pagina'] - $esquerda > 2) {
+				$html .= '<li class="disabled"><span>&hellip;</span></li>';
+			}
+		}
+
+		// Paginas anteriores a corrente
+		for ($i = $esquerda; $i > 0; $i--)
+		{
+			$p = $paginacao['pagina'] - $i;
+			$html .= sprintf(
+				'<li><a href="%s"><span class="sr-only">Página</span> %s</a></li>',
+				call_user_func($paginacao['callback_link'], $paginacao, $p),
+				number_format($p, 0, ',', '.')
+			);
+		}
+
+		// Pagina corrente
 		$html .= sprintf(
-			'<li class="active"><span>%s</span></li>',
+			'<li class="active"><span><span class="sr-only">Página</span> %s</span></li>',
 			number_format($paginacao['pagina'], 0, ',', '.')
 		);
 
+		// Paginas posteiores a corrente
+		for ($i = 1; $i <= $direita; $i++)
+		{
+			$p = $paginacao['pagina'] + $i;
+			$html .= sprintf(
+				'<li><a href="%s"><span class="sr-only">Página</span> %s</a></li>',
+				call_user_func($paginacao['callback_link'], $paginacao, $p),
+				number_format($p, 0, ',', '.')
+			);
+		}
+
+		// Ultima pagina
+		if ($paginacao['pagina'] + $direita < $paginacao['ultima_pagina']) {
+			if ($paginacao['pagina'] + $direita < $paginacao['ultima_pagina'] - 1) {
+				$html .= '<li class="disabled"><span>&hellip;</span></li>';
+			}
+			$html .= sprintf(
+				'<li><a href="%s" title="Última página"><span class="sr-only">Página</span> %s</a></li>',
+				call_user_func($paginacao['callback_link'], $paginacao, $paginacao['ultima_pagina']),
+				number_format($paginacao['ultima_pagina'], 0, ',', '.')
+			);
+		}
+
+		// Pagina seguinte
 		if ($paginacao['pagina'] < $paginacao['ultima_pagina'])
 		{
 			$html .= sprintf(
