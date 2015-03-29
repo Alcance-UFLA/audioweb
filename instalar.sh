946876362076f27590aaabf9ffd874846d003d56
Rubens Takiguti Ribeiro <rubs33@gmail.com>
2015-03-29 01:17:43 -0300

    Ajustes no instalador.

--- instalar.sh
+++ instalar.sh
@@ -1,24 +1,48 @@
 #!/bin/bash
 PHPEXEC=$(type -P php)
+CURLEXEC=$(type -P curl)
 if [ ! -f "$PHPEXEC" ]
 then
-    echo "O php-cli nao foi encontrado"
-    exit 1
+	echo "O php-cli nao foi encontrado"
+	exit 1
+fi
+if [ ! -f "$CURLEXEC" ]
+then
+	echo "O comando curl nao foi encontrado"
+	exit 1
 fi
 
 if [ ! -f "composer.phar" ]
 then
-    echo "INSTALANDO O COMPOSER"
-    curl -sS https://getcomposer.org/installer | php
+	echo "INSTALANDO O COMPOSER"
+	"$CURLEXEC" -sS "https://getcomposer.org/installer" | "$PHPEXEC"
 fi
 
-echo "INSTALANDO O AUDIOWEB PELO COMPOSER"
-php "composer.phar" install
-
-if (( $? == 0 ))
+if [ ! -f "composer.lock" ]
 then
-    echo "FIM DA INSTALACAO COM SUCESSO"
+
+	echo "INSTALANDO O AUDIOWEB PELO COMPOSER"
+	"$PHPEXEC" "composer.phar" install
+
+	if (( $? == 0 ))
+	then
+		echo "FIM DA INSTALACAO COM SUCESSO"
+	else
+		echo "ERRO DURANTE A INSTALACAO"
+		exit 1
+	fi
+
 else
-    echo "ERRO DURANTE A INSTALACAO"
-    exit 1
+
+	echo "ATUALIZANDO O AUDIOWEB PELO COMPOSER"
+	"$PHPEXEC" "composer.phar" update
+
+	if (( $? == 0 ))
+	then
+		echo "FIM DA ATUALIZACAO COM SUCESSO"
+	else
+		echo "ERRO DURANTE A ATUALIZACAO"
+		exit 1
+	fi
+
 fi
