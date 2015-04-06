0e7a399a82bf6bd31f596733e91f2ce03b679978
Rubens Takiguti Ribeiro <rubs33@gmail.com>
2015-04-05 21:49:33 -0300

    Ajuste da config de e-mail.

--- application/config/audioweb.php
+++ application/config/audioweb.php
@@ -4,9 +4,9 @@ return array(
 	'versao' => '0.3',
 	'tamanho_limite_upload' => '2M',
 	'cdn' => Kohana::$base_url,
-	'email_sistema' => 'audioweb@aw.polarisweb.com.br',
+	'email_sistema' => 'contato@aw.polarisweb.com.br',
 	'nome_sistema' => 'AudioWeb',
-	'email_desenvolvimento' => 'audioweb@polarisweb.com.br',
+	'email_desenvolvimento' => 'contato@awdev.polarisweb.com.br',
 	'compactar_html' => 0,
 	'usar_etag' => false,
 );
